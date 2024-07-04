b58457dac8d9159d3d85d0fb8117dc7f9a8be2b0
astonuser <astonuser@MacBook-Pro-astadmin.local>
2024-07-04 20:09:25 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("dsadas");
+        System.out.println("dsadsadasdas");
+        // Single source of truth
+    }
+}
